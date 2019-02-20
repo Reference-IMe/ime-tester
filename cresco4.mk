f8e246090ffd3db9d0e5444012ff85a3bdd36a91
Marcello <marcello@local>
2019-02-20 20:13:50 +0100

    amended again makefiles for cresco4 with icc
    Former-commit-id: 0a6d1cca5d63e935dfbbc981918607f4e9fd2660

--- cresco4.mk
+++ cresco4.mk
@@ -1,7 +1,7 @@
 PROJECT_DIR = $(CURDIR)
 BIN_DIR = $(PROJECT_DIR)/bin
 SRC_DIR = $(PROJECT_DIR)/src
-NO_WARN_UNUSED = -Wno-unused-but-set-variable -Wno-unused-result -Wno-unused-variable
+NO_WARN_UNUSED = -Wno-unused-but-set-variable -Wno-unused-variable
 
 export
 
--- src/testers/cresco4.mk
+++ src/testers/cresco4.mk
@@ -1,4 +1,4 @@
-CC = gcc
+CC = icc
 MPICC = mpicc
 DEBUG = -g -O3
 CFLAGS = -Wall $(NO_WARN_UNUSED) $(DEBUG)
@@ -8,11 +8,10 @@ all : testers
 testers : SLGEWOS-compare SLGEWOPV-compare
   
 SLGEWOS-compare : SLGEWOS-compare.c
-	$(CC) $(CFLAGS) SLGEWOS-compare.c -o $(BIN_DIR)/SLGEWOS-compare -llapack -lblas
+	$(CC) $(CFLAGS) SLGEWOS-compare.c -o $(BIN_DIR)/SLGEWOS-compare -mkl -lmkl_sequential -ldl
 
 SLGEWOPV-compare : SLGEWOPV-compare.c
-#	$(MPICC) $(CFLAGS) SLGEWOPV-compare.c -o $(BIN_DIR)/SLGEWOPV-compare /usr/lib/x86_64-linux-gnu/libmpi.so /usr/lib/x86_64-linux-gnu/libscalapack-openmpi.so.2.0 /usr/lib/x86_64-linux-gnu/libblacsCinit-openmpi.a /usr/lib/x86_64-linux-gnu/libblacs-openmpi.a
-	$(MPICC) $(CFLAGS) SLGEWOPV-compare.c -o $(BIN_DIR)/SLGEWOPV-compare  -lmkl_scalapack_lp64  -lmkl_blacs_openmpi_lp64 -mkl
+	$(MPICC) $(CFLAGS) SLGEWOPV-compare.c -o $(BIN_DIR)/SLGEWOPV-compare -mkl -lmkl_scalapack_lp64 -lmkl_blacs_openmpi_lp64 -ldl
 
 all : testers
 
