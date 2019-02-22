d08a0036b22d5455711f475e80cf04eac472ac47
Marcello <marcello@local>
2019-02-22 17:28:17 +0100

    amended makefile for marconi-knl
    Former-commit-id: ce19d89d0e8c79c42ba051ced35af5a6be97d3b2

--- src/testers/marconi-knl.mk
+++ src/testers/marconi-knl.mk
@@ -1,18 +1,21 @@
-
 CC = icc
-MPICC = mpicc
+MPICC = mpiicc
 DEBUG = -g -O3
 CFLAGS = -Wall $(NO_WARN_UNUSED) $(DEBUG)
-
 all : testers
 
 testers : SLGEWOS-compare SLGEWOPV-compare
   
 SLGEWOS-compare : SLGEWOS-compare.c
-        $(CC) $(CFLAGS) SLGEWOS-compare.c -o $(BIN_DIR)/SLGEWOS-compare -lifcore -I$(LAPACK_INC) -L$(LAPACK_LIB) -llapack -I$(BLAS_INC) -L$(BLAS_LIB) -lblas
+	$(CC) $(CFLAGS) SLGEWOS-compare.c -o $(BIN_DIR)/SLGEWOS-compare -I$(MKL_INC) -L$(MKL_LIB) -lmkl_intel_lp64 -lmkl_core -lmkl_sequential -lm
+
 
 SLGEWOPV-compare : SLGEWOPV-compare.c
-        $(MPICC) $(CFLAGS) SLGEWOPV-compare.c -o $(BIN_DIR)/SLGEWOPV-compare  -I$(MKL_INC) -L$(MKL_LIB) -lmkl_scalapack_lp64 -lmkl_intel_lp64 -lmkl_blacs_intelmpi_lp64 -lmkl_core -lmkl_sequential -lm
+	$(MPICC) $(CFLAGS) SLGEWOPV-compare.c -o $(BIN_DIR)/SLGEWOPV-compare  -I$(MKL_INC) -L$(MKL_LIB) -lmkl_scalapack_lp64 -lmkl_intel_lp64 -lmkl_blacs_intelmpi_lp64 -lmkl_core -lmkl_sequential -lm
+
+all : testers
+
+
 
 clean:
-        \rm *.o *.a *~ *.so
+	\rm *.o *.a *~ *.so
