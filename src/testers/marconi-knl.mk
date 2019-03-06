d7c50c4014fea23e6dd105069022990b5107ff4d
Marcello <marcello@local>
2019-03-06 16:35:12 +0100

    new folder structure
    renamed scripts
    
    Former-commit-id: 8e90614404c8b6211c3021135ce1b2234995d155

--- src/testers/marconi-knl.mk
+++ src/testers/marconi-knl.mk
@@ -2,6 +2,7 @@ CC = icc
 MPICC = mpiicc
 DEBUG = -g -O3
 CFLAGS = -Wall $(NO_WARN_UNUSED) $(DEBUG)
+
 all : testers
 
 testers : SLGEWOS-compare SLGEWOPV-compare
@@ -12,10 +13,6 @@ SLGEWOS-compare : SLGEWOS-compare.c
 
 SLGEWOPV-compare : SLGEWOPV-compare.c
 	$(MPICC) $(CFLAGS) SLGEWOPV-compare.c -o $(BIN_DIR)/SLGEWOPV-compare  -I$(MKL_INC) -L$(MKL_LIB) -lmkl_scalapack_lp64 -lmkl_intel_lp64 -lmkl_blacs_intelmpi_lp64 -lmkl_core -lmkl_sequential -lm
-
-all : testers
-
-
 
 clean:
 	\rm *.o *.a *~ *.so
