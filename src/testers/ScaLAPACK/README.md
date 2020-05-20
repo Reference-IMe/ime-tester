1417700d3103bdeca2baa8dae7c580e76a258ae5
Marcello <marcello@local>
2020-05-20 20:25:39 +0200

    show detailed timings of scalapack LU
    Former-commit-id: ad8d31ec52eb09e6f6d4578f6db1e82c5a12fba1

--- src/testers/ScaLAPACK/README.md
+++ src/testers/ScaLAPACK/README.md
@@ -11,3 +11,8 @@ source: [netlib](http://netlib.org/scalapack/slug/node106.html#SECTION0451100000
 - Use efficient machine-specific BLAS (not the Fortran 77 reference implementation BLAS) and BLACS (nondebug, BLACSDBGLVL=0 in Bmake.inc).
 
 at Cineca: [example code](http://www.hpc.cineca.it/content/scalapack-solution-0) and [how to compile](https://hpc-forge.cineca.it/files/ScuolaCalcoloParallelo_WebDAV/public/anno-2016/12_Advanced_School/scalapack_short.pdf)
+
+### distributing matrices
+https://stackoverflow.com/questions/30167724/how-to-use-pdgemr2d-to-copy-distributed-matrix-in-total-to-all-processes/30358361#30358361
+- problem with 32 bit integers when large matrices
+- low performance of pdgemr2d
---
+++ src/testers/ScaLAPACK/ScaLAPACK_pDGETRF.showtime.h
@@ -0,0 +1,171 @@
+/*
+ * ScaLAPACK_pDGETRF.h
+ *
+ *  Created on: May 15, 2020
+ *      Author: marcello
+ */
+
+#include <mpi.h>
+#include <time.h>
+#include "../../helpers/macros.h"
+#include "../../helpers/matrix.h"
+#include "../../helpers/vector.h"
+#include "../../helpers/Cblacs.h"
+#include "../../helpers/scalapack.h"
+#include "../tester_structures.h"
+
+#define SHOWTIME(step) printf("%d(%d):%f\n",mpi_rank,step,MPI_Wtime()-result.total_start_time);
+
+test_output ScaLAPACK_pDGETRF(int n, double* A_global, double* B_global, int nb,
+								int mpi_rank, int cprocs,
+								int nprow, int npcol, int myrow, int mycol,
+								int context, int context_global)
+{
+	test_output result = EMPTY_OUTPUT;
+
+	result.total_start_time = MPI_Wtime(); // time(NULL);
+	SHOWTIME(1)
+
+	// general
+	int i;
+	int i0 = 0;
+	int i1 = 1;
+	int m = 1;
+	double d0 = 0.0;
+	double d1 = 1.0;
+	int info;
+	int *ipiv;
+
+	// matrix
+	int nr, nc;
+	double *A;
+	double *At;
+	int ncrhs, nrrhs;
+	double *B;
+	int ncrhst, nrrhst;
+	double *Bt;
+
+	int descA_global[9];
+	int descB_global[9];
+	int descA[9];
+	int descAt[9];
+	int descB[9];
+	int descBt[9];
+
+	int lld, lldt;
+
+
+	if (mpi_rank < cprocs)
+	{
+		// Computation of local matrix size
+		nc = numroc_( &n, &nb, &mycol, &i0, &npcol );
+		nr = numroc_( &n, &nb, &myrow, &i0, &nprow );
+		lld = MAX( 1 , nr );
+		A = malloc(nr*nc*sizeof(double));
+		At = malloc(nr*nc*sizeof(double));
+
+		ncrhs = numroc_( &m, &nb, &mycol, &i0, &npcol );
+		nrrhs = numroc_( &n, &nb, &myrow, &i0, &nprow );
+		B = malloc(nrrhs*ncrhs*sizeof(double));
+
+		ncrhst = numroc_( &n, &nb, &mycol, &i0, &npcol );
+		nrrhst = numroc_( &m, &nb, &myrow, &i0, &nprow );
+		lldt = MAX( 1 , nrrhst );
+		Bt = malloc(nrrhst*ncrhst*sizeof(double));
+
+		ipiv = malloc((lld+nb)*sizeof(int));
+
+		// Descriptors (local)
+		descinit_( descA, &n, &n, &nb, &nb, &i0, &i0, &context, &lld, &info );
+		descinit_( descAt, &n, &n, &nb, &nb, &i0, &i0, &context, &lld, &info );
+		descinit_( descB, &n, &m, &nb, &nb, &i0, &i0, &context, &lld, &info );
+		descinit_( descBt, &m, &n, &nb, &nb, &i0, &i0, &context, &lldt, &info );
+
+		if (mpi_rank==0)
+		{
+			// Descriptors (global)
+			descinit_( descA_global, &n, &n, &i1, &i1, &i0, &i0, &context_global, &n, &info );
+			descinit_( descB_global, &n, &m, &i1, &i1, &i0, &i0, &context_global, &n, &info );
+		}
+		else
+		{
+			// Descriptors (global, for non-root nodes)
+			for (i=0; i<9; i++)
+			{
+				descA_global[i]=0;
+				descB_global[i]=0;
+			}
+			descA_global[1]=-1;
+			descB_global[1]=-1;
+		}
+		SHOWTIME(2)
+		// spread A
+		pdgemr2d_(&n, &n, A_global, &i1, &i1, descA_global, A, &i1, &i1, descA, &context);
+		SHOWTIME(3)
+		// transpose A
+		pdtran_(&n, &n, &d1, A, &i1, &i1, descA, &d0, At, &i1, &i1, descAt);
+		SHOWTIME(4)
+		// LU factorization
+		result.core_start_time = MPI_Wtime();
+		//pdgetrf_( &n, &n, At, &i1, &i1, descAt, ipiv, &info );
+		result.core_end_time = MPI_Wtime();
+		result.exit_code = info;
+		SHOWTIME(5)
+		// transpose back A
+		pdtran_(&n, &n, &d1, At, &i1, &i1, descAt, &d0, A, &i1, &i1, descA);
+		SHOWTIME(6)
+		// get back A
+		pdgemr2d_ (&n, &n, A, &i1, &i1, descA, A_global, &i1, &i1, descA_global, &context);
+		SHOWTIME(7)
+		result.total_end_time = time(NULL);
+
+		/*
+		 * evaluate n.r.e.
+		 */
+		// spread B
+		/*
+		pdgemr2d_(&n, &m, B_global, &i1, &i1, descB_global, B, &i1, &i1, descB, &context);
+
+		pdgetrs_("N",  &n, &m, At, &i1, &i1, descAt, ipiv, B, &i1, &i1, descB, &info  );
+			/*
+			 * no need of transposing back B, being an 1D vector
+			 */
+			/*
+			// re-transpose result
+			pdtran_(&m, &n, &d1, B, &i1, &i1, descB, &d0, Bt, &i1, &i1, descBt);
+
+			// collect result
+			if (mpi_rank==0)
+			{
+				// Adapt descriptor for B to accept transposed matrix
+				descinit_( descB_global, &m, &n, &i1, &i1, &i0, &i0, &context_global, &m, &info );
+			}
+
+			// get back result
+			pdgemr2d_(&m, &n, Bt, &i1, &i1, descBt, B_global, &i1, &i1, descB_global, &context);
+			 */
+		pdgemr2d_(&n, &m, B, &i1, &i1, descB, B_global, &i1, &i1, descB_global, &context);
+	}
+	else
+	{
+		A  = NULL;
+		At = NULL;
+		B  = NULL;
+		Bt = NULL;
+		ipiv = NULL;
+
+		result.core_start_time = MPI_Wtime();
+		result.core_end_time = MPI_Wtime();
+		result.exit_code = 0;
+	}
+
+	// cleanup
+	NULLFREE(A);
+	NULLFREE(At);
+	NULLFREE(B);
+	NULLFREE(Bt);
+	NULLFREE(ipiv);
+
+	MPI_Barrier(MPI_COMM_WORLD);
+	return result;
+}
