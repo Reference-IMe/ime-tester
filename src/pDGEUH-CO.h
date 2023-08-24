8690bcd564306c32c533ff2413a0d5c9f9f184a9
Marcello <marcello@local>
2023-08-24 12:13:58 +0200

    prepared files to handle other precisions
    Former-commit-id: 00220305e5b1b742f9c747539ad6280d2a4bdc4a

--- src/pDGEUH-CO.h
+++ src/pDGEUH-CO.h
@@ -7,17 +7,6 @@ __attribute__((always_inline)) inline void pbDGEUH_CO (	int mpi_rank_row_in_col,
 														int last_row,
 														double* lastKr, double* lastKc, double* h)
 {
-	int i;
-
-	// chunks of last row and col are symmetrical to the main diagonal
-	// they are hosted on procs on the diagonal
-	// last_row = myrows | l_row | 0
-	// last_row = ( mpi_rank_row_in_col < l_owner )*myrows + ( mpi_rank_row_in_col == l_owner )*l_row;
-
-	#pragma GCC ivdep
-	for (i=0; i<last_row; i++)
-	{
-		h[i]   = 1/(1-lastKc[i]*lastKr[i]);
-	}
+	#include "p_GEUH-CO.inc"
 }
 #endif
